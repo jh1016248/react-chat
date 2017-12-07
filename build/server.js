d58ea3073e1ca65c048403da50f255fbb8fa6914
jh <1774643394@qq.com>
2017-12-07 19:35:51 +0800

    update

--- build/server.js
+++ build/server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var webpack = require('webpack');
-var webpackConfig = require('./webpack.config');
+var webpack = require('webpack')
+var webpackConfig = require('../webpack.config')
 var compiler = webpack(webpackConfig)
+var app = require('./router')
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
     publicPath: webpackConfig.output.publicPath,
@@ -12,11 +12,7 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler, {
     log: () => {}
 })
 
-var app = express();
-
 app.use(devMiddleware)
 app.use(hotMiddleware)
-
 app.listen(3000)
-
 console.log('listen 3000')
